ef0b2608b8eae1c417b2d71419fe4386061cfa1d
Андрей <An5375drey@mail.ru>
2020-11-10 16:09:54 +0300

    добавил свой феесбук и свой инстаграмм, дополнил вк, сделал кнопки

--- main.py
+++ main.py
@@ -2,25 +2,54 @@ from vk_api.vk_api import VkApi
 import facebook
 from instagram.client import InstagramAPI
 import requests
-name = input('введите название соц. сети: ')
-user_id = input('введите id пользователя: ')
-if name == 'vk':
-    vk = VkApi(app_id=7646807, token='1bfbce991bfbce991bfbce99991b8f60ce11bfb1bfbce9944664b4da26b585a50448b66').get_api()
-    data = vk.users.get(user_ids=user_id, fields=['city', 'country', 'education', 'interests','about'])[0]
-    print('профиль закрыт' if data['is_closed'] else 'профиль открыт')
-    print(data['city']['title'] if 'city' in data else 'город не указан')
-    print(data['country']['title'] if 'country' in data else 'страна не указан')
-    print(data['education'] if 'education' in data else 'Образование не указано')
-    print(data['interests'] if 'interests' in data else 'Нет интересов')
-elif name == 'inst':
-    replay = requests.get('https://graph.instagram.com/'+user_id+'?fields=id,username&access_token='+'IGQVJVbndKX19uTDZAkT0pLeHpjejloR2p5eWdZAQkRtZA0xLWDdrZAmRmVVE2bURSa01jb2NMdmZAIYVFXRWlOamUyWUFCZAUp1SWZAvcEhrTV9uSGtnU043bWJ2MkpOaGV3aTFWbTF5N0czRDZA4NW00RnJaaQZDZD')
-    print(replay.text)
-elif name == 'facebook':
-    graph = facebook.GraphAPI(access_token='EAAJ0cmAkx5MBABcfic57UmHwaBqguHE1FhhgQRU6ExlaEaz3uiN62hc62gNPE7HLHVCzolDEInWZALuEwpYrlZAZALA93xGhlZBZCJ30dLP0P6GZCj99K1BH6pkn5tBwV7redbXaZBYw2ZAZAEru3g3ZAlB2Nivu572RKl3NHAoZCCZBov9dK2ZCcSBBTrupEMoLZBctExJgEjoM6ZC9gZDZD')
-    user_fields = graph.get_object(user_id, fields='location{location{city,country}}')
-    location = user_fields.get('location',{}).get('location',{})
-    print(location['city'] if 'city' in location else 'город не указан')
-    print(location['country'] if 'country' in location else 'страна не указана')
-else:
-    print('нихрена ты умный, свали с такими высказываниями отсюда')
+from tkinter import *
+def func():
+    social = var.get()
+    user_id = ent.get()
+    if social == 0:
+        vk = VkApi(app_id=7646807, token='1bfbce991bfbce991bfbce99991b8f60ce11bfb1bfbce9944664b4da26b585a50448b66').get_api()
+        data = vk.users.get(user_ids=user_id, fields=['city', 'country', 'education', 'interests','about'])[0]
+        output = ''
+        output += ('профиль закрыт' if data['is_closed'] else 'профиль открыт') + '\n'
+        output += (data['city']['title'] if 'city' in data else 'город не указан') + '\n'
+        output += (data['country']['title'] if 'country' in data else 'страна не указан') + '\n'
+        output += (data['education'] if 'education' in data else 'Образование не указано') + '\n'
+        output += (data['interests'] if 'interests' in data else 'Нет интересов') + '\n'
+        lab2['text'] = output
+    elif social == 1:
+        output2 = ''
+        replay = requests.get('https://graph.instagram.com/'+user_id+'?fields=id,username&access_token='+'IGQVJVbndKX19uTDZAkT0pLeHpjejloR2p5eWdZAQkRtZA0xLWDdrZAmRmVVE2bURSa01jb2NMdmZAIYVFXRWlOamUyWUFCZAUp1SWZAvcEhrTV9uSGtnU043bWJ2MkpOaGV3aTFWbTF5N0czRDZA4NW00RnJaaQZDZD')
+        output2 += (replay.text) + '\n'
+        lab2['text'] = output2
+    elif social == 2:
+        output3 = ''
+        graph = facebook.GraphAPI(access_token='EAAJ0cmAkx5MBAJsBcNj84Gq6Jxbp5kC03GzVNuKPVN6cFNKfWnIkaN4pIuDYC5L0U44YQyy4s4ImUnIwmwZB6GgXKLcA3QPksEmYTR1FhyxoqZAPHyZB1nsI9jLoxeNEOTfcfhqxkBLJxZABmZCXZCxNJ7iSvEOo7sdTK4hB522hvsaindAyexWHI91rR0EYLw1Mqdajl2Rag6sZBx22xTnGv2y5rDBqSUVnyzexZAlHWJoZB9RsNxgtJBm4qMWx36IAZD')
+        user_fields = graph.get_object(user_id, fields='location{location{city,country}}')
+        location = user_fields.get('location', {}).get('location', {})
+        output3 += (location['city'] if 'city' in location else 'город не указан') + '\n'
+        output3 += (location['country'] if 'country' in location else 'страна не указана') + '\n'
+        lab2['text'] = output3
 
+
+root = Tk()
+root.geometry("600x600")
+
+var = IntVar()
+lab = Label(text='Choose social')
+rb1 = Radiobutton(text='VK', variable=var, value=0)
+rb2 = Radiobutton(text='Instagram', variable=var, value=1)
+rb3 = Radiobutton(text='Facebook', variable=var, value=2)
+lab1 = Label(text='Enter ID:')
+ent = Entry()
+btn = Button(text='Confirum', command=func)
+lab2 = Label()
+
+lab.pack()
+rb1.pack()
+rb2.pack()
+rb3.pack()
+lab1.pack()
+ent.pack()
+btn.pack()
+lab2.pack()
+root.mainloop()
